a4c81fb0a80a1b554339418ffa5e1136b9103b70
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-01-22 10:58:08 -0700

    pff

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,6 @@ export default function middleware(request: Request) {
 	const pathname = new URL(request.url).pathname;
 
 	console.log('URL in middleware: ', request.url);
-	console.log('rewriting to: ', new URL(request.url).pathname + '/rewritten');
-	return rewrite(pathname + '/rewritten');
+	console.log('rewriting to: ', new URL(request.url).pathname + '-rewritten');
+	return rewrite(pathname + '-rewritten');
 }
