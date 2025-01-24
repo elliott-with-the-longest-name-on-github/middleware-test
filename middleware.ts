62a511c2f82fb14116bea5cac98f9958b1b8d148
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-01-23 21:27:23 -0700

    test

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
 import { rewrite, next } from '@vercel/edge';
 
 export default function middleware(request: Request) {
-	const pathname = new URL(request.url).pathname;
-
 	console.log('URL in middleware: ', request.url);
-	console.log('rewriting to: ', new URL(request.url).pathname + '-rewritten');
-	return rewrite(pathname + '-rewritten');
+	console.log('rewriting to: ', '/rewritten');
+	return rewrite('/rewritten');
 }
