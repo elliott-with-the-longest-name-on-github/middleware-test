576f8784f298b6778e50d4b89cd23a1c98ed1da8
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-01-30 14:06:51 -0700

    repro vc dev

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { rewrite, next } from '@vercel/edge';
-
-export default function middleware(request: Request) {
-	console.log('URL in middleware: ', request.url);
-	console.log('rewriting to: ', '/rewritten');
-	return rewrite('/rewritten');
-}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,4 @@
 export function handle({ event, resolve }) {
-	console.log(
-		'SvelteKit sees this:',
-		'url',
-		event.request.url,
-		'headers',
-		Object.fromEntries(event.request.headers.entries())
-	);
+	console.log('in server request');
 	return resolve(event);
 }
-
